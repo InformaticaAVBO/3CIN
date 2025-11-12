78ef859086e5ac9632947168a77a98a76e7ce5b9
Sandro Gallo <5992750+sandrogallo@users.noreply.github.com>
2025-11-12 08:33:11 +0000

    12/11

--- P01_HelloWorld/HelloWorld.cpp
+++ P01_HelloWorld/HelloWorld.cpp
@@ -4,11 +4,30 @@
 */
 
 #include <iostream>
+#include <cmath>
 using namespace std;
+
+double reale2percepita( double T, double v ) {
+    if (v<5) return T;
+    double percecipita = ( 33 + (0.45+0.29*sqrt(v) - 0.02*v) * (T-33) );
+    return percecipita;  
+}
+
+bool isPalindrome( string s ) {
+    int lunghezza = s.length();
+    for (int i=0; i<lunghezza/2; i++) {
+        if (s[i] != s[lunghezza-(i+1)]) return false;
+    }
+    return true;
+}
+
+
 
 int main()
 {
-    cout<<"Hello World" << endl;
+    cout << isPalindrome("Hello World") << endl;
+    cout << isPalindrome("ciaoaic") << endl;
+    cout << reale2percepita(30,10) << endl;
 
     return 0;
 }
